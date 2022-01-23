93c36f1f30c7edb99234b67a37f5aed1f93122b8
eduardo escobar <war_army_10@hotmail.com>
2022-01-23 14:18:09 -0600

    Adding simple execution

--- src/main/java/com/perfectbogus/dev/Main.java
+++ src/main/java/com/perfectbogus/dev/Main.java
@@ -4,5 +4,16 @@ public class Main {
 
     public static void main(String[] args){
 
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("We are now in thread " + Thread.currentThread().getName());
+            }
+        });
+
+        System.out.println("We are in thread: " + Thread.currentThread().getName());
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName());
+
     }
 }
