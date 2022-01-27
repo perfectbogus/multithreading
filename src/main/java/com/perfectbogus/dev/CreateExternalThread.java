007c9d67266f0fc13b577553932d75fbfee5a2ba
eduardo escobar <war_army_10@hotmail.com>
2022-01-26 22:53:31 -0600

    Chapter 5

---
+++ src/main/java/com/perfectbogus/dev/CreateExternalThread.java
@@ -0,0 +1,17 @@
+package com.perfectbogus.dev;
+
+public class CreateExternalThread {
+
+    public static void main(String[] args){
+        Thread thread = new ExternalClassThread();
+
+        thread.start();
+    }
+
+    private static class ExternalClassThread extends Thread{
+        @Override
+        public void run(){
+            System.out.println("Hello From: " + Thread.currentThread().getName());
+        }
+    }
+}
