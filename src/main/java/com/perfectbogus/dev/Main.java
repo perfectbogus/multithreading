83243bf3d6ba9c7b399225eab914268e08533012
eduardo escobar <war_army_10@hotmail.com>
2022-01-26 22:30:35 -0600

    Chapter 4: Thread Creation

--- src/main/java/com/perfectbogus/dev/Main.java
+++ src/main/java/com/perfectbogus/dev/Main.java
@@ -7,9 +7,13 @@ public class Main {
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                System.out.println("We are now in thread " + Thread.currentThread().getName());
+                System.out.println("We are now in thread: " + Thread.currentThread().getName());
+                System.out.println("Current Thread Priority: " + Thread.currentThread().getPriority());
             }
         });
+
+        thread.setName("New Worker Thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
 
         System.out.println("We are in thread: " + Thread.currentThread().getName());
         thread.start();
