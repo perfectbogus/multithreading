a203cda65035563a819e26a72e3039cdf6342d9a
eduardo escobar <war_army_10@hotmail.com>
2022-02-19 12:35:54 -0600

    Chapter 21: Avoid Circular Deadlock

---
+++ src/main/java/com/perfectbogus/dev/DeadLockExample.java
@@ -0,0 +1,93 @@
+package com.perfectbogus.dev;
+
+import java.util.Random;
+
+public class DeadLockExample {
+
+    public static void main(String[] args) {
+        Intersection intersection = new Intersection();
+        Thread trainAThread = new Thread(new TrainA(intersection));
+        Thread trainBThread = new Thread(new TrainB(intersection));
+
+        trainAThread.start();
+        trainBThread.start(); 
+    }
+
+    public static class TrainB implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainB(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTime = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+
+    public static class TrainA implements Runnable {
+        private Intersection intersection;
+        private Random random = new Random();
+
+        public TrainA(Intersection intersection) {
+            this.intersection = intersection;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                long sleepingTIme = random.nextInt(5);
+                try {
+                    Thread.sleep(sleepingTIme);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                intersection.takeRoadB();
+            }
+        }
+    }
+
+    public static class Intersection {
+        private Object roadA = new Object();
+        private Object roadB = new Object();
+
+        public void takeRoadA(){
+            synchronized (roadA) {
+                System.out.println("Road A is locked by thread: " + Thread.currentThread().getName());
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road A");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        public void takeRoadB(){
+            synchronized (roadA) {
+                System.out.println("Road B is locked by thread: " + Thread.currentThread().getName());
+                synchronized (roadB) {
+                    System.out.println("Train is passing through road B");
+                    try {
+                        Thread.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
